2cbb8337750e8c7395b5747262d424c5cc32b3e2
Dinesh <dineshkr4114@gmail.com>
2022-03-13 11:32:11 +0530

    LocAuth Git Created

---
+++ app/src/main/java/com/example/locauth/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.locauth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=findViewById(R.id.button2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Halloooo");
+            }
+        });
+    }
+}
